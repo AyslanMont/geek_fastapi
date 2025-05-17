dca8a6ab2d2694adc2d83c3a78c5905f9050c1f2
Ayslan Francisco Monteiro Neto <ayslanfrancisco999@gmail.com>
2025-05-17 13:29:06 -0300

    aula: Método POST

--- secao03/src/secao03/main.py
+++ secao03/src/secao03/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import status
+from src.secao03.models import Curso
 
 app = FastAPI()
 
@@ -30,3 +31,11 @@ async def get_curso(curso_id: int):
         return curso
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
+    
+@app.post("/cursos", status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+    curso_id = len(cursos) + 1
+    curso.id = curso_id
+    cursos[curso_id] = curso 
+
+    return curso
