9dac69650d69a5e7a58bada57063ad6a8e0316c3
Ayslan Francisco Monteiro Neto <ayslanfrancisco999@gmail.com>
2025-05-17 13:58:49 -0300

    aula: Método PUT

--- secao03/src/secao03/main.py
+++ secao03/src/secao03/main.py
@@ -5,37 +5,42 @@ from src.secao03.models import Curso
 
 app = FastAPI()
 
-cursos = {
-    1: {
-        "titulo": "Programação para Leigos",
-        "aulas": 112,
-        "horas": 89
-    },
-    2: {
-        "titulo": "Algoritmos e Lógica de Programação",
-        "aulas": 110,
-        "horas": 76
-    },
-}
+cursos = dict()
 
-@app.get("/cursos")
+
+@app.get("/cursos/")
 async def get_cursos():
     return cursos
+
 
 @app.get("/cursos/{curso_id}")
 async def get_curso(curso_id: int):
     try:
         curso = cursos[curso_id]
         curso.update({"id": curso_id})
-        
+
         return curso
     except KeyError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
-    
-@app.post("/cursos", status_code=status.HTTP_201_CREATED)
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado!")
+
+
+@app.post("/cursos/{curso_id}", status_code=status.HTTP_201_CREATED)
 async def post_curso(curso: Curso):
     curso_id = len(cursos) + 1
     curso.id = curso_id
-    cursos[curso_id] = curso 
+    cursos[curso_id] = curso
 
     return curso
+
+
+@app.put("/cursos/{curso_id}/")
+async def put_curso(curso_id: int, curso: Curso):
+    if curso_id in cursos:
+        curso.id = curso_id
+        cursos[curso_id] = curso
+
+        return curso
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado!")
